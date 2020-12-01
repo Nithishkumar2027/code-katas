64fbcc9f7f7e1846685bc12e6f881e33ee9084e2
Nithish kumar <mnnithishkumar20@gmail.com>
2020-12-01 12:43:27 +0530

    Solved

---
+++ 7 Kyu problems/Jaden Casing Strings/jadenCasing.js
@@ -0,0 +1,30 @@
+// Solution #1
+String.prototype.toJadenCase = function () {
+    let jadenCase = '';
+    let capitalize = true;
+
+    for (let i = 0; i < this.length; i++) {
+        const letter = this[i];
+        if (letter === ' ') {
+            capitalize = true;
+            jadenCase += ' ';
+        } else {
+            if (capitalize) {
+                jadenCase += letter.toUpperCase();
+                capitalize = false;
+            } else {
+                jadenCase += letter.toLowerCase();
+            }
+        }
+    }
+    return jadenCase;
+}
+
+// Solution #2
+String.prototype.toJadenCase = function () {
+    return this.toString().split(' ').map(word => word[0].toUpperCase() + word.substr(1)).join(' ')
+};
+
+
+var str = "How are you";
+console.log(str.toJadenCase());
